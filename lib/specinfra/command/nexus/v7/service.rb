f1b246138b876bd1c0f70487f69b80610a3c43dd
Carl Perry <caperry@edolnx.net>
2015-09-01 11:53:42 -0500

    Fix typos

--- lib/specinfra/command/nexus/v7/service.rb
+++ lib/specinfra/command/nexus/v7/service.rb
@@ -1,5 +1,5 @@
 class Specinfra::Command::Nexus::V7::Service < Specinfra::Command::Nexus::Base::Service
   class << self
-    include Specinfra::Command::Module::Init
+    include Specinfra::Command::Module::Service::Init
   end
 end
--- lib/specinfra/helper/detect_os/nexus.rb
+++ lib/specinfra/helper/detect_os/nexus.rb
@@ -1,11 +1,9 @@
 class Specinfra::Helper::DetectOs::Nexus < Specinfra::Helper::DetectOs
   def detect
-    print "!!>> Nexus Module"
     # CentOS has a os-release file too, but this should do the right thing
     if File.exists?('/etc/os-release')
       contents = {}
       File.read('/etc/os-release').split.collect {|x| x.split('=')}.each {|x| contents[x[0]] = x[1]}
-      print "!!>> Inside Nexus Detection"
       # This test should only work on a nexus platform in the native OS
       # It will fail in a guest/container shell, which should be picked up elsewhere
       if contents['CISCO_RELEASE_INFO'] && contents['CISCO_RELEASE_INFO'] == "/etc/os-release"
